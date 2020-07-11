de52a8818328b8b6b04cf2040a46a4968e7d6bd0
sayyidmaulana <sayyidmaulanaofficial@gmail.com>
2020-07-12 03:13:39 +0700

    Initial Commit

---
+++ DicodingFundamental/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DicodingFundamental
+//
+//  Created by Sayyid Maulana Khakul Y on 12/07/20.
+//  Copyright © 2020 Dicoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
